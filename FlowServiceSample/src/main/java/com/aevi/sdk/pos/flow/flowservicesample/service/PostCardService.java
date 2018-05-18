7c8271fd7c9856d5f1ddf3df0b8c3bf41e68d119
brettwold <brett@annalytics.co.uk>
2018-05-18 09:07:31 +0100

    Change to extend correct service and call correct Activity

--- FlowServiceSample/src/main/java/com/aevi/sdk/pos/flow/flowservicesample/service/PostCardService.java
+++ FlowServiceSample/src/main/java/com/aevi/sdk/pos/flow/flowservicesample/service/PostCardService.java
@@ -1,15 +1,15 @@
 package com.aevi.sdk.pos.flow.flowservicesample.service;
 
 
-import com.aevi.sdk.pos.flow.flowservicesample.ui.PostPaymentActivity;
-import com.aevi.sdk.pos.flow.model.TransactionSummary;
-import com.aevi.sdk.pos.flow.service.BasePostPaymentService;
+import com.aevi.sdk.pos.flow.flowservicesample.ui.PrePaymentActivity;
+import com.aevi.sdk.pos.flow.model.TransactionRequest;
+import com.aevi.sdk.pos.flow.service.BasePrePaymentService;
 
-public class PostCardService extends BasePostPaymentService {
+public class PostCardService extends BasePrePaymentService {
 
     @Override
-    protected void processRequest(String clientMessageId, TransactionSummary transactionSummary) {
-        launchActivity(PostPaymentActivity.class, clientMessageId, transactionSummary);
+    protected void processRequest(String clientMessageId, TransactionRequest transactionRequest) {
+        launchActivity(PrePaymentActivity.class, clientMessageId, transactionRequest);
     }
 
     @Override
