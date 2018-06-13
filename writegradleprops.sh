3d3ace82d09339c68f77b1585130495916219af6
Andreas Borglin <andreas@jakoon.co.uk>
2018-06-13 15:54:44 +0100

    Added bintray credentials to writegradleprops.sh

--- writegradleprops.sh
+++ writegradleprops.sh
@@ -19,3 +19,5 @@ echo "platform_store_password=$AEVI_PLATFORM_STORE_PASSWORD" >> ${OUTPUT_PROPS}
 echo "keystore_key_password=$AEVI_KEYSTORE_KEY_PASSWORD" >> ${OUTPUT_PROPS}
 echo "aws_accessid=$AEVI_ARTIFACTORY_AWS_ACCESS_ID" >> ${OUTPUT_PROPS}
 echo "aws_accesskey=$AEVI_ARTIFACTORY_AWS_SECRET" >> ${OUTPUT_PROPS}
+echo "bintrayUser=$BINTRAY_USER" >> ${OUTPUT_PROPS}
+echo "bintrayKey=$BINTRAY_KEY" >> ${OUTPUT_PROPS}
