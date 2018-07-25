a15d965766f9cb404c7bec712c89e8d87fac0e91
Andreas Borglin <andreas@jakoon.co.uk>
2018-07-25 13:21:21 +0100

    Minor fixes to help layouts and styling

--- sample-commons/src/main/java/com/aevi/sdk/pos/flow/sample/ui/HelpActivity.java
+++ sample-commons/src/main/java/com/aevi/sdk/pos/flow/sample/ui/HelpActivity.java
@@ -16,6 +16,7 @@ package com.aevi.sdk.pos.flow.sample.ui;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.Window;
 import android.widget.TextView;
 
 import com.aevi.sdk.pos.flow.sample.R;
@@ -28,6 +29,7 @@ public class HelpActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        supportRequestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.activity_help);
 
         TextView helpText = findViewById(R.id.text);
