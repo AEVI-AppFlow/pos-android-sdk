b28385a69a4623b119cdcb8aa5a3a68eadc9a3ca
Andreas Borglin <andreas@jakoon.co.uk>
2018-11-26 15:42:45 +0000

    Added menu controls to generic initiation activity

--- PaymentInitiationSample/src/main/java/com/aevi/sdk/pos/flow/paymentinitiationsample/ui/RequestInitiationActivity.java
+++ PaymentInitiationSample/src/main/java/com/aevi/sdk/pos/flow/paymentinitiationsample/ui/RequestInitiationActivity.java
@@ -15,14 +15,23 @@
 package com.aevi.sdk.pos.flow.paymentinitiationsample.ui;
 
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import butterknife.BindView;
 import butterknife.ButterKnife;
+import com.aevi.sdk.flow.model.Request;
 import com.aevi.sdk.pos.flow.paymentinitiationsample.R;
+import com.aevi.sdk.pos.flow.paymentinitiationsample.ui.fragment.GenericRequestFragment;
+import com.aevi.sdk.pos.flow.sample.ui.BaseSampleAppCompatActivity;
 import com.aevi.sdk.pos.flow.sample.ui.ModelDisplay;
 
-public class RequestInitiationActivity extends AppCompatActivity {
+public class RequestInitiationActivity extends BaseSampleAppCompatActivity {
 
     private ModelDisplay modelDisplay;
+
+    private GenericRequestFragment genericRequestFragment;
+
+    @BindView(R.id.toolbar)
+    Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,9 +39,76 @@ public class RequestInitiationActivity extends AppCompatActivity {
         setContentView(R.layout.activity_request);
         ButterKnife.bind(this);
         modelDisplay = (ModelDisplay) getSupportFragmentManager().findFragmentById(R.id.fragment_request_details);
+        if (modelDisplay != null) {
+            modelDisplay.showTitle(false);
+        }
+        genericRequestFragment = (GenericRequestFragment) getSupportFragmentManager().findFragmentById(R.id.fragment_request);
+        setupToolbar(toolbar, R.string.initiate_request);
     }
+
 
     public ModelDisplay getModelDisplay() {
         return modelDisplay;
     }
+
+    @Override
+    protected String getModelTitle() {
+        return getString(R.string.request_data);
+    }
+
+    @Override
+    protected boolean showFlowStagesOption() {
+        return false;
+    }
+
+    @Override
+    protected boolean showViewRequestOption() {
+        return false;
+    }
+
+    @Override
+    protected boolean showViewModelOption() {
+        return !getResources().getBoolean(R.bool.dualPane);
+    }
+
+    @Override
+    protected int getPrimaryColor() {
+        return getResources().getColor(R.color.colorPrimary);
+    }
+
+    @Override
+    protected String getCurrentStage() {
+        return "Generic initiation";
+    }
+
+    @Override
+    protected Class<?> getRequestClass() {
+        return Request.class;
+    }
+
+    @Override
+    protected Class<?> getResponseClass() {
+        return Request.class;
+    }
+
+    @Override
+    protected String getModelJson() {
+        return genericRequestFragment.getRequest().toJson();
+    }
+
+    @Override
+    protected String getRequestJson() {
+        return null;
+    }
+
+    @Override
+    protected String getHelpText() {
+        return getString(R.string.generic_initiation_help);
+    }
+
+    @Override
+    protected boolean allowBack() {
+        return true;
+    }
+
 }
