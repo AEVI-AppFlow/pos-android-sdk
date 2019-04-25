c26811d8b09bbc5826dfe50e2b6e1b0d1acd370c
Andreas Borglin <andreas@jakoon.co.uk>
2019-04-25 08:55:50 +0100

    Disable additional amounts options in sample that are already in the request

--- FlowServiceSample/src/main/java/com/aevi/sdk/pos/flow/flowservicesample/ui/BasePreProcessingActivity.java
+++ FlowServiceSample/src/main/java/com/aevi/sdk/pos/flow/flowservicesample/ui/BasePreProcessingActivity.java
@@ -16,6 +16,7 @@ package com.aevi.sdk.pos.flow.flowservicesample.ui;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 import butterknife.BindView;
 import butterknife.BindViews;
@@ -51,8 +52,11 @@ abstract class BasePreProcessingActivity extends BaseSampleAppCompatActivity {
     @BindViews({R.id.pay_with_points, R.id.pay_with_giftcard, R.id.discount_basket_item})
     List<View> payViews;
 
+    @BindView(R.id.add_charity)
+    Button charityView;
+
     @BindView(R.id.add_surcharge)
-    TextView surchargeView;
+    Button surchargeView;
 
     @BindView(R.id.pay_with_giftcard)
     TextView giftCardView;
@@ -65,6 +69,13 @@ abstract class BasePreProcessingActivity extends BaseSampleAppCompatActivity {
 
         preTransactionModel = PreTransactionModel.fromActivity(this);
         transactionRequest = preTransactionModel.getTransactionRequest();
+
+        if (transactionRequest.getAmounts().hasAdditionalAmount(AmountIdentifiers.AMOUNT_CHARITY_DONATION)) {
+            charityView.setEnabled(false);
+        }
+        if (transactionRequest.getAmounts().hasAdditionalAmount(AmountIdentifiers.AMOUNT_SURCHARGE)) {
+            surchargeView.setEnabled(false);
+        }
 
         surchargeView.setText(getString(R.string.add_surcharge_fee,
                                         AmountFormatter.formatAmount(transactionRequest.getAmounts().getCurrency(), getResources()
